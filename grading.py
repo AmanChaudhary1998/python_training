da5752fe993b47831a3f32407ef1ff0b1dd8d79b
AmanChaudhary1998 <44546228+AmanChaudhary1998@users.noreply.github.com>
2020-01-22 10:18:06 +0530

    Add files via upload

--- grading.py
+++ grading.py
@@ -1,13 +1,11 @@
-n = int(input("enter your marks below 100\n"))
-if(n>=90 and n<100):
-     print("Your grade is A")
-elif(n<90 and n>=80):
-     print("your grade is B")
-elif(n<80 and n>=70):
-     print("your grade is C")
-elif(n<70 and n>=60):
-     print("your grade is D")
-elif(n<60):
-     print("you are fail")
+score=int(input("enter the score"))
+if (score>=90):
+    print("your grade is A")
+elif(score>=80):
+    print("your grade is B")
+elif(score>=70):
+    print("your grade is C")
+elif(score>=60):
+    print("your grade is D")
 else:
-     print("Invalid input")
+    print("your grade is F")
